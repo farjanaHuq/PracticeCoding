99c808c8cf1375b2468ac33938adfba0edb0807e
Farjana Huq <fa.huq03@gmail.com>
2019-10-03 08:10:50 -0700

    Problem 9 updated

--- StringnArray/Problem9.js
+++ StringnArray/Problem9.js
@@ -6,22 +6,25 @@ function findELementZero(matrix, m, n){
     for(let i=0; i<m; i++){
         for(let j=0; j<n;j++){
             if(matrix[i][j]===0){
-                return convertElementsToZero(matrix);
+                var row = i;
+                var col = j;
+                return convertElementsToZero(matrix, row, col);
             }
         }
     }
 
-    function convertElementsToZero(matrix){
-        for(let i=0; i<m; i++){
-            for(let j=0; j<n;j++){
-                matrix[i][j] = 0;
-            }
-        }
-       // console.log(matrix);
+    function convertElementsToZero(matrix,row,col){
+       for(let i=0; i<n; i++){ 
+           matrix[row][i] = 0;
+       }
+       for(let j=0;j<m;j++){
+           matrix[j][col] = 0;
+       }
         return matrix;
     }
 }
 
-const testMatrix = [[1, 2, 0], [4, 5, 6], [7, 8, 9]];
+//const testMatrix = [[1, 2, 3], [4, 0, 6], [7, 8, 9]];
+const testMatrix = [[1, 2, 3], [4, 0, 6], [7, 8, 0]];
 
 console.log(findELementZero(testMatrix, 3,3));
