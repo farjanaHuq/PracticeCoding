0ef742392d41be8d79a00b4ec2f5b7223f7d0bac
Farjana Huq <fa.huq03@gmail.com>
2019-11-26 11:34:16 -0800

    Stack logic created

--- DataStructure/Stack/stack.js
+++ DataStructure/Stack/stack.js
@@ -3,21 +3,26 @@ class Stack{
         this.storage = [];
         this.length = 0;
     }
+    getLength(){
+        return this.length;
+    }
     push(data){
         this.storage[this.length] = data;
         this.length++;
     }
     pop(){
-       this.storage[--this.length] = null;   
+       this.storage[this.length--] = null;  
+
     }
     //returns last value added to the slack
     peek(){
-       return this.storage[--this.length];
+       return this.storage[this.length-1];
     }
     
     isEmpty(){
         return this.length === 0;
     }
+  
 }
 
 
@@ -28,11 +33,29 @@ myStack.push(5);
 myStack.push(6);
 myStack.push(7);
 myStack.push(8);
+
+
+
+console.log(myStack);
 //pop()
+myStack.pop();
 
+//console.log(`Stack after pop() ${myStack}`);
+console.log(`Last element added ${myStack.peek()}`);
+console.log(`Is empty: ${myStack.isEmpty()}`);
+console.log(`Length ${myStack.getLength()}`);
+console.log('===================================================');
+//pop()
+myStack.pop();
 
-console.log(myStack);
-myStack.pop()
-console.log(myStack);
-console.log(myStack.peek());
-console.log(myStack.isEmpty());
+console.log(`Last element added ${myStack.peek()}`);
+console.log(`Is empty: ${myStack.isEmpty()}`);
+console.log(`Length ${myStack.getLength()}`);
+
+console.log('===================================================');
+//pop()
+myStack.pop();
+
+console.log(`Last element added ${myStack.peek()}`);
+console.log(`Is empty: ${myStack.isEmpty()}`);
+console.log(`Length ${myStack.getLength()}`);
